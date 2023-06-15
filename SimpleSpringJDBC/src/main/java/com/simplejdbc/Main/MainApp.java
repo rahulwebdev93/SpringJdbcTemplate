c9c91ca65704c0f4562160d45d2d465ca97ad839
rahulwebdev93 <rahul.webdev93@gmail.com>
2023-06-15 22:22:59 +0530

    firstcommit

--- SimpleSpringJDBC/src/main/java/com/simplejdbc/Main/MainApp.java
+++ SimpleSpringJDBC/src/main/java/com/simplejdbc/Main/MainApp.java
@@ -14,7 +14,7 @@ public class MainApp {
 		EmployeeDaoImpl employeeDaoimpl = ctx.getBean("EmployeeDaoImpl", EmployeeDaoImpl.class);
 
 		Employee emp = new Employee();
-		emp.setName("rahul");
+		emp.setName("name");
 		emp.setDepartment("it");
 		emp.setSalary("1000");
 		
